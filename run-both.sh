f5c325bb19653c9fb29b825eb95c2ee154fad9f9
rgordon <rgordon0@gmail.com>
2020-05-04 00:09:28 -0700

    let script pull latest data

--- run-both.sh
+++ run-both.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-Rscript -e "Sys.setenv(RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc') ; rmarkdown::render('log-analysis.Rmd')"
+git pull
+pushd ../covid-19-data
+git pull
+popd
+
+Rscript -e "Sys.setenv(RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc') ; rmarkdown::render('quick-analysis.Rmd')"
 Rscript -e "Sys.setenv(RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc') ; rmarkdown::render('log-change-analysis.Rmd')"
